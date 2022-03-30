b49dcf970c1d14eda92a4a76b2d5a1b1669629b5
gaelle1701 <gaellecoue@gmail.com>
2022-03-30 17:29:31 +0200

    add home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  // ngOnInit(): void {
+  // }
 
 }
